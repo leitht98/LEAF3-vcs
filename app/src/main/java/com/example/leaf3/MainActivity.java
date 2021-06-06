b75b070dc4458cea05df23c352d003b0e949bd04
leight98 <thomasjleith@gmail.com>
2021-06-06 17:59:25 +0100

    Turned off gps attempt

--- app/src/main/java/com/example/leaf3/MainActivity.java
+++ app/src/main/java/com/example/leaf3/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.leaf3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
@@ -19,13 +26,37 @@ import java.util.Date;
 
 import static java.lang.Math.log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener{
+    Location currentLocation = new Location("");
+    protected LocationManager locationManager;
+    protected LocationListener locationListener;
+    protected String latitude,longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("HI");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (LocationListener) this);*/
         new MyTask().execute();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        System.out.println("!Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
+        currentLocation = location;
     }
 
     private class MyTask extends AsyncTask<Void, Void, Void> {
@@ -33,8 +64,10 @@ public class MainActivity extends AppCompatActivity {
         String result;
         @Override
         protected Void doInBackground(Void... voids) {
+            System.out.println("Doing anything?");//apparently not
             String daysOutput = "Days required: "+daysToReachUVDose();
             System.out.println(daysOutput);
+            System.out.println("Latitude:" + currentLocation.getLatitude() + ", Longitude:" + currentLocation.getLongitude());
             return null;
         }
 

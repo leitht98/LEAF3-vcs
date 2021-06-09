52d3cb27ebc8f419338ab96fcf4949d42fbe4cb3
leight98 <thomasjleith@gmail.com>
2021-06-09 18:09:12 +0100

    not sure? Just checking I've saved the latest version

--- app/src/main/java/com/example/leaf3/MainActivity.java
+++ app/src/main/java/com/example/leaf3/MainActivity.java
@@ -1,21 +1,35 @@
 package com.example.leaf3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Looper;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -39,10 +53,16 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
     protected String latitude,longitude;
     protected float uvFen;
     TextView resultOutput;
+    FusedLocationProviderClient mFusedLocationClient;
+
+    TextView latitudeTextView, longitTextView;
+    int PERMISSION_ID = 44;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        System.out.println("HI");
+        //System.out.println("HI");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -53,7 +73,14 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
         coveringSpinner.setAdapter(adapter);
         coveringSpinner.setOnItemSelectedListener(this);
 
+        latitudeTextView = findViewById(R.id.latTextView);
+        longitTextView = findViewById(R.id.lonTextView);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
         resultOutput = (TextView) findViewById(R.id.result_output);
+
+        getLastLocation();
 
         /*
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
@@ -71,9 +98,127 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
         //new MyTask().execute();
     }
 
+    //https://www.geeksforgeeks.org/how-to-get-user-location-in-android/
+    //Very much copied
+    @SuppressLint("MissingPermission")
+    private void getLastLocation() {
+        // check if permissions are given
+        if (checkPermissions()) {
+
+            // check if location is enabled
+            if (isLocationEnabled()) {
+
+                // getting last
+                // location from
+                // FusedLocationClient
+                // object
+                mFusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        Location location = task.getResult();
+                        if (location == null) {
+                            requestNewLocationData();
+                        } else {
+                            latitudeTextView.setText(location.getLatitude() + "");
+                            longitTextView.setText(location.getLongitude() + "");
+                            //System.out.println("~~~~~~~~~~~~Lat:"+location.getLatitude() + "");
+                            //System.out.println("~~~~~~~~~~~~Long:"+location.getLongitude() + "");
+                            latitude = location.getLatitude()+"";
+                            longitude = location.getLongitude()+"";
+                        }
+                    }
+                });
+            } else {
+                Toast.makeText(this, "Please turn on" + " your location...", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        } else {
+            // if permissions aren't available,
+            // request for permissions
+            requestPermissions();
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void requestNewLocationData() {
+
+        // Initializing LocationRequest
+        // object with appropriate methods
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(5);
+        mLocationRequest.setFastestInterval(0);
+        mLocationRequest.setNumUpdates(1);
+
+        // setting LocationRequest
+        // on FusedLocationClient
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+    }
+
+    private LocationCallback mLocationCallback = new LocationCallback() {
+
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            Location mLastLocation = locationResult.getLastLocation();
+            latitudeTextView.setText("Latitude: " + mLastLocation.getLatitude() + "");
+            longitTextView.setText("Longitude: " + mLastLocation.getLongitude() + "");
+            //System.out.println("~~~~~~~Lat:"+mLastLocation.getLatitude() + "");
+            //System.out.println("~~~~~~~Long:"+mLastLocation.getLongitude() + "");
+            //latitude = mLastLocation.getLatitude()+"";
+            //longitude = mLastLocation.getLongitude()+"";
+        }
+    };
+
+    // method to check for permissions
+    private boolean checkPermissions() {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+
+        // If we want background location
+        // on Android 10.0 and higher,
+        // use:
+        // ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+
+    // method to request for permissions
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_ID);
+    }
+
+    // method to check
+    // if location is enabled
+    private boolean isLocationEnabled() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    // If everything is alright then
+    @Override
+    public void
+    onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSION_ID) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastLocation();
+            }
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (checkPermissions()) {
+            getLastLocation();
+        }
+    }
+
     public void onItemSelected(AdapterView<?> parent, View view, int pos, long id){
         parent.getItemAtPosition(pos);
-        System.out.println("---------------------------------------------------------------------------" + id);
+        //System.out.println("---------------------------------------------------------------------------" + id);
         switch ((int)id) {
             case 0: //t
                 uvFen = (float) 0.035078273;
@@ -89,7 +234,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
                 break;
 
         }
-        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!FEN" +uvFen);
+        //System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!FEN" +uvFen);
         new MyTask().execute();
     }
     public void onNothingSelected(AdapterView<?> parent) {
@@ -100,7 +245,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
 
     @Override
     public void onLocationChanged(Location location) {
-        System.out.println("!Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
+        //System.out.println("!Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
         currentLocation = location;
     }
 
@@ -111,7 +256,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
         @Override
         protected Void doInBackground(Void... voids) {
 
-            System.out.println("Doing anything?");//apparently not
+            //System.out.println("Doing anything?");
             String daysOutput = "";
             if (daysToReachUVDose()>300){
                 daysOutput = "Days required: OVER 300";
@@ -120,7 +265,8 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
             }
             resultOutput.setText(daysOutput);
             System.out.println(daysOutput);
-            System.out.println("Latitude:" + currentLocation.getLatitude() + ", Longitude:" + currentLocation.getLongitude());
+            //System.out.println("Latitude:" + currentLocation.getLatitude() + ", Longitude:" + currentLocation.getLongitude());
+            //System.out.println("!Latitude:" + latitude + ", !Longitude:" + longitude);
             return null;
         }
 
@@ -176,11 +322,15 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
                 requestDate = java.sql.Date.valueOf(lRequestDate.toString());
                 SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
                 String dateString = formatter.format(requestDate);
-                System.out.println(dateString);
-                System.out.println("!!!!!!!!!!------"+uvFen);
-                String latString = "55.953251";
-                String longString = "-3.188267";
-                url = new URL("https://api.sunrise-sunset.org/json?lat="+latString+"&lng="+longString+"&date="+dateString);
+                //System.out.println(dateString);
+                //System.out.println("!!!!!!!!!!------"+uvFen);
+                String latString = "0";
+                String longString = "0";
+                //System.out.println("########################"+latitude+longitude);
+                //System.out.println("### "+"https://api.sunrise-sunset.org/json?lat="+latString+"&lng="+longString+"&date="+dateString);
+                //System.out.println("# "+"https://api.sunrise-sunset.org/json?lat="+latitude+"&lng="+longitude+"&date="+dateString);
+                url = new URL("https://api.sunrise-sunset.org/json?lat="+latitude+"&lng="+longitude+"&date="+dateString);
+                System.out.println(url);
                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()));
                 String stringBuffer;
                 String stringOutput = "";

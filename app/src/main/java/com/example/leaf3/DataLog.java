a336b7ea4b55c3ffd50509d417f8ececc214a053
Pascal <pascal.queeney@gmail.com>
2021-06-11 04:58:10 +0100

    who cares no one will see this :)

--- app/src/main/java/com/example/leaf3/DataLog.java
+++ app/src/main/java/com/example/leaf3/DataLog.java
@@ -1,15 +1,16 @@
 package com.example.leaf3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class DataLog extends AppCompatActivity {
     Button backButton;
-    TextView projectData;
     String dataString;
 
     @Override
@@ -17,13 +18,37 @@ public class DataLog extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_log);
         backButton = findViewById(R.id.backButton);
-        projectData = findViewById(R.id.projectData);
 
+        //Data stuff
+
+        //get the list
+        RecyclerView projectDataList = findViewById(R.id.dataList);
+
+        //Find and tidy data
         Intent intent = getIntent();
         dataString = intent.getStringExtra("data_string");
-        projectData.setText(dataString);
+        String[] dataArray = dataString.split("\\{");
+        for (int i=0; i<dataArray.length; i++){
+            //gets rid of a trailing bracket (used the other to split it)
+            dataArray[i] = dataArray[i].replace("}", "");
+            //adds new lines
+            dataArray[i] = dataArray[i].replace(", ", "\n");
+            //removes dead space
+            dataArray[i] = dataArray[i].trim();
+        }
 
-        backButton.setOnClickListener(v->{
+        //get a new instance of a very basic custom adapter (taken from the google tutorials, mostly)
+        DataAdapter dataAdapter = new DataAdapter(dataArray);
+        //add the adapter to the list thing
+        projectDataList.setAdapter(dataAdapter);
+        //make a layout manager and add it
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        projectDataList.setLayoutManager(layoutManager);
+        //make it sexy
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(projectDataList.getContext(), layoutManager.getOrientation());
+        projectDataList.addItemDecoration(dividerItemDecoration);
+
+        backButton.setOnClickListener(v -> {
             this.finish();
         });
     }
